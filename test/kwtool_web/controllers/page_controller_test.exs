4e0a57acc19f2c3bfd5bf3e66c2f513a12d9ff54
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-23 17:42:01 +0700

    [1] Update test for the homepage

--- test/kwtool_web/controllers/page_controller_test.exs
+++ test/kwtool_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule KwtoolWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Keyword research tool"
   end
 end
