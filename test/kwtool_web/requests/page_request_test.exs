b854b12a2c77227587cd64039e391cfdb5067c7e
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-25 10:55:30 +0700

    [1] Add request test

--- test/kwtool_web/requests/page_request_test.exs
+++ test/kwtool_web/requests/page_request_test.exs
@@ -1,10 +1,11 @@
-defmodule KwtoolWeb.PageControllerTest do
+defmodule KwtoolWeb.Requests.PageRequestTest do
   use KwtoolWeb.ConnCase
 
   describe "get the home route" do
     test "returns 200 status when access the home route", %{conn: conn} do
-      conn = get(conn, "/")
-      assert html_response(conn, 200) =~ "Keyword Research Tool"
+      conn = get(conn, Routes.page_path(conn, :index))
+
+      assert conn.status == 200
     end
   end
 end
