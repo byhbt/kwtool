1ca3b5cf62bfd7bf4e84ea9f06c90b24c1f00645
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-25 09:11:36 +0700

    [1] Refactor pipe chain start with a raw value

--- lib/kwtool_web/views/layout_view.ex
+++ lib/kwtool_web/views/layout_view.ex
@@ -6,7 +6,8 @@ defmodule KwtoolWeb.LayoutView do
   end
 
   defp module_class_name(conn) do
-    controller_module(conn)
+    conn
+    |> controller_module()
     |> Phoenix.Naming.resource_name("Controller")
     |> String.replace("_", "-")
   end
