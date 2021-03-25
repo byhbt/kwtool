75a6213c7aab4403836102d04dfd38d63e15dc59
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-25 09:22:37 +0700

    [1] Update assert value

--- test/kwtool_web/controllers/page_controller_test.exs
+++ test/kwtool_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule KwtoolWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Keyword research tool"
+    assert html_response(conn, 200) =~ "Keyword Research Tool"
   end
 end
