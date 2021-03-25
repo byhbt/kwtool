34d1419e34d85b51b407908d459a46137c0a5631
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-25 07:15:14 +0700

    [1] Update scss directory structure

--- lib/kwtool_web/views/layout_view.ex
+++ lib/kwtool_web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule KwtoolWeb.LayoutView do
   use KwtoolWeb, :view
+
+  def page_class_name(conn) do
+    "#{module_class_name(conn)} #{action_name(conn)}"
+  end
+
+  defp module_class_name(conn) do
+    controller_module(conn)
+    |> Phoenix.Naming.resource_name("Controller")
+    |> String.replace("_", "-")
+  end
 end
