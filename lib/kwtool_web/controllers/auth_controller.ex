94986ab09251e39b8fbb46ef9c13354b9ccdbc0c
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-26 13:40:03 +0700

    [3] Set layout for the Authentication controller

--- lib/kwtool_web/controllers/auth_controller.ex
+++ lib/kwtool_web/controllers/auth_controller.ex
@@ -1,9 +1,9 @@
 defmodule KwtoolWeb.AuthController do
   use KwtoolWeb, :controller
 
+  plug :put_layout, "auth.html"
+
   def sign_up(conn, _params) do
-    render(conn, "sign_up.html",
-      layout: { KwtoolWeb.LayoutView, "auth.html" }
-    )
+    render(conn, "sign_up.html")
   end
 end
