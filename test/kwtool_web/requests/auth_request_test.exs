c82fb8290ba5f76e7512aa095abaeb45fb0dd1b6
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-26 15:44:33 +0700

    [3] Remove empty line

--- test/kwtool_web/requests/auth_request_test.exs
+++ test/kwtool_web/requests/auth_request_test.exs
@@ -4,7 +4,6 @@ defmodule KwtoolWeb.Requests.AuthRequestTest do
   describe "get the sign up route" do
     test "returns 200 status", %{conn: conn} do
       conn = get(conn, Routes.auth_path(conn, :sign_up))
-
       assert conn.status == 200
     end
   end
