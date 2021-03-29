f457cc46820ebc619473e8c4e2fbdd577468eb68
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-29 15:26:23 +0700

    [3] Rename the describe test convention

--- test/kwtool_web/controllers/auth_controller_test.exs
+++ test/kwtool_web/controllers/auth_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule KwtoolWeb.AuthControllerTest do
   use KwtoolWeb.ConnCase, async: true
 
-  describe "get the sign up route" do
+  describe "sign_up/2" do
     test "returns 200 status", %{conn: conn} do
       conn = get(conn, "/sign_up")
       assert html_response(conn, 200) =~ "Keyword Research Tool"
