ca468e86d7b49d8b0870d1dbee4afdb6d5c8ac37
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-29 18:03:57 +0700

    [4] Add validation rules

--- lib/kwtool/accounts/user.ex
+++ lib/kwtool/accounts/user.ex
@@ -3,20 +3,21 @@ defmodule Kwtool.Accounts.User do
   import Ecto.Changeset
 
   schema "users" do
-    field :email, :string, unique: true, null: false
-    field :full_name, :string, null: false
+    field :email, :string
+    field :full_name, :string
     field :company, :string
-    field :password, :string, null: false
-    field :confirm_password, :string, null: false
-
+    field :password, :string
+    field :confirm_password
 
     timestamps()
   end
 
-  @doc false
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:email, :full_name, :company, :password, :confirm_password])
-    |> validate_required([:email, :full_name, :company, :password, :confirm_password])
+    |> validate_required([:email, :full_name, :password, :confirm_password])
+    |> unique_constraint(:email)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 4)
   end
 end
