033ec6daa5a15d2e99b610eb382c9f8d72761203
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-29 15:19:24 +0700

    [4] Handle form submit

--- lib/kwtool_web/controllers/auth_controller.ex
+++ lib/kwtool_web/controllers/auth_controller.ex
@@ -1,6 +1,9 @@
 defmodule KwtoolWeb.AuthController do
   use KwtoolWeb, :controller
 
+  alias Kwtool.Accounts
+  alias Kwtool.Accounts.User
+
   plug :put_layout, "auth.html"
 
   def new(conn, _params) do
@@ -8,10 +11,19 @@ defmodule KwtoolWeb.AuthController do
   end
 
   def show(conn, _params) do
-    render(conn, "sign_up.html")
+    changeset = Accounts.change_user(%User{})
+    render(conn, "sign_up.html", changeset: changeset)
   end
 
-  def create_user(conn, _params) do
-    IO.puts "Create user!"
+  def create_user(conn, %{"user" => user_params}) do
+    case Accounts.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User created successfully.")
+        |> redirect(to: Routes.user_path(conn, :show, user))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
