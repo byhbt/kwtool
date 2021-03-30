7efe5feb8d3c1dde0fb505812cff987a54c06101
Thanh Huynh <huynhbathanh@gmail.com>
2021-03-30 14:35:20 +0700

    [3] Update description of request test

--- test/kwtool_web/requests/auth_request_test.exs
+++ test/kwtool_web/requests/auth_request_test.exs
@@ -1,7 +1,7 @@
 defmodule KwtoolWeb.Requests.AuthRequestTest do
   use KwtoolWeb.ConnCase
 
-  describe "/sign_up" do
+  describe "get /sign_up" do
     test "returns 200 status", %{conn: conn} do
       conn = get(conn, Routes.auth_path(conn, :show))
       assert conn.status == 200
