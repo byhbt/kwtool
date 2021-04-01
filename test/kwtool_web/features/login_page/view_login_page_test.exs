323a486cd39def6e3117abcae66ec969b723f2bb
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-01 10:01:38 +0700

    [22] Implement frontend view

---
+++ test/kwtool_web/features/login_page/view_login_page_test.exs
@@ -0,0 +1,10 @@
+defmodule KwtoolWeb.HomePage.ViewLoginPageTest do
+  use KwtoolWeb.FeatureCase, async: true
+
+  @path Routes.auth_path(KwtoolWeb.Endpoint, :index)
+
+  feature "view login page", %{session: session} do
+    visit(session, @path)
+    assert_has(session, Query.text("Login"))
+  end
+end
