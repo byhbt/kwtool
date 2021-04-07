3c8816b8194fc79788740fddc7ede1583da32ab5
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-07 13:56:54 +0700

    [24] Add integration test for the login

---
+++ test/kwtool_web/controllers/session_controller_test.exs
@@ -0,0 +1,51 @@
+defmodule KwtoolWeb.SessionControllerTest do
+  use KwtoolWeb.ConnCase, async: true
+
+  describe "get show/2" do
+    test "returns 200 status", %{conn: conn} do
+      conn = KwtoolWeb.SessionController.call(conn, :show)
+
+      assert html_response(conn, 200) =~ "Registration"
+    end
+  end
+
+  describe "get sign_in/2" do
+    test "returns 200 status", %{conn: conn} do
+      conn = KwtoolWeb.AuthController.call(conn, :new)
+
+      assert html_response(conn, 200) =~ "Login"
+    end
+  end
+
+  describe "post sign_in/2" do
+    test "redirects to home page when the registration data is valid", %{conn: conn} do
+      account_params = %{email: "john@example.com", password: "123456"}
+
+      conn = post(conn, Routes.auth_path(conn, :update), user: params_for(:user, account_params))
+
+      assert redirected_to(conn) == Routes.page_path(conn, :index)
+      assert get_flash(conn, :info) == "Logged in successfully!"
+    end
+  end
+
+  describe "post create/2" do
+    test "redirects to home page when the registration data is valid", %{conn: conn} do
+      password_inputs = %{password: "123456", password_confirmation: "123456"}
+
+      conn = post(conn, Routes.auth_path(conn, :create), user: params_for(:user, password_inputs))
+
+      assert redirected_to(conn) == Routes.page_path(conn, :index)
+      assert get_flash(conn, :info) == "Your account is created successfully!"
+    end
+
+    test "renders errors when given invalid registration params", %{conn: conn} do
+      override_email_attr = %{email: "invalid_email"}
+
+      conn =
+        post(conn, Routes.auth_path(conn, :create), user: params_for(:user, override_email_attr))
+
+      assert html_response(conn, 200) =~
+               "Oops, something went wrong! Please check the errors below."
+    end
+  end
+end
