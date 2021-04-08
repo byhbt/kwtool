5692165e56109282876535e4beb45a23bd1b2eb8
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-08 11:17:02 +0700

    [24] Add failed login test case

--- test/kwtool_web/controllers/session_controller_test.exs
+++ test/kwtool_web/controllers/session_controller_test.exs
@@ -10,7 +10,7 @@ defmodule KwtoolWeb.SessionControllerTest do
   end
 
   describe "post sign_in/2" do
-    test "redirects to home page when the registration data is valid", %{conn: conn} do
+    test "redirects to the home page when the login params is valid", %{conn: conn} do
       attrs = %{email: "john.does@example.com"}
       insert(:user, attrs)
 
@@ -19,6 +19,17 @@ defmodule KwtoolWeb.SessionControllerTest do
 
       assert redirected_to(conn) == Routes.home_path(conn, :index)
       assert get_flash(conn, :info) == "Welcome back!"
+    end
+
+    test "renders the login page when the login params is NOT valid", %{conn: conn} do
+      attrs = %{email: "john.does@example.com"}
+      insert(:user, attrs)
+
+      login_form_params = %{email: "someone.else@example.com", password: "123456"}
+      conn = post(conn, Routes.session_path(conn, :create), user: login_form_params)
+
+      assert html_response(conn, 200) =~ "Login"
+      assert get_flash(conn, :error) == "Incorrect email or password"
     end
   end
 end
