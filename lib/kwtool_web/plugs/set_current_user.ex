ec6dacf4f2c440b41511541070bedbc370520adc
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-12 16:04:47 +0700

    [24] Format code

--- lib/kwtool_web/plugs/set_current_user.ex
+++ lib/kwtool_web/plugs/set_current_user.ex
@@ -1,10 +1,12 @@
 defmodule KwtoolWeb.Plugs.SetCurrentUser do
   import Plug.Conn
 
+  alias Kwtool.Accounts.Guardian.Plug
+
   def init(_options), do: nil
 
   def call(conn, _params) do
-    current_user = Kwtool.Accounts.Guardian.Plug.current_resource(conn)
+    current_user = Plug.current_resource(conn)
 
     if current_user do
       conn
