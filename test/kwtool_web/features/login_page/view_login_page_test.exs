682aa2fd621292820a1eb867551f65f406c5f674
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-19 14:30:07 +0700

    [6] Add feature test for dashboard

--- test/kwtool_web/features/login_page/view_login_page_test.exs
+++ test/kwtool_web/features/login_page/view_login_page_test.exs
@@ -4,9 +4,9 @@ defmodule KwtoolWeb.HomePage.ViewLoginPageTest do
   @path Routes.session_path(KwtoolWeb.Endpoint, :new)
 
   feature "views login page", %{session: session} do
-    visit(session, @path)
-
-    assert_has(session, Query.text("Login"))
+    session
+    |> visit(@path)
+    |> assert_has(Query.text("Login"))
   end
 
   feature "signs in user with valid information", %{session: session} do
@@ -17,7 +17,7 @@ defmodule KwtoolWeb.HomePage.ViewLoginPageTest do
     |> visit(@path)
     |> fill_in(Query.text_field("Email"), with: "john.does@example.com")
     |> fill_in(Query.css("#user_password"), with: "123456")
-    |> click(Wallaby.Query.button("Sign in"))
+    |> click(Query.button("Sign in"))
     |> assert_has(Query.text("Welcome back!"))
   end
 end
