039b10cafd1074550649a9dda5a163222cf61237
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-19 17:53:15 +0700

    [29] Implement keyword parse method

--- lib/kwtool_web/controllers/upload_controller.ex
+++ lib/kwtool_web/controllers/upload_controller.ex
@@ -1,17 +1,11 @@
 defmodule KwtoolWeb.UploadController do
   use KwtoolWeb, :controller
 
-  alias NimbleCSV.RFC4180, as: CSV
-
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def create(conn, %{"keyword_file" => %Plug.Upload{}=keyword_file}) do
-    list =
-    keyword_file.path
-    |> File.stream!
-    |> CSV.parse_stream(skip_headers: false)
-    |> Enum.each(&IO.puts/1)
+    list = Kwtool.Crawlers.import_from_file(keyword_file)
   end
 end
