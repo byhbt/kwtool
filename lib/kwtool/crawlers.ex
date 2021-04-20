b8234b26134f013907269379f6c343e00ceb70c4
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-20 16:36:36 +0700

    [29] Add test for upload controller

--- lib/kwtool/crawlers.ex
+++ lib/kwtool/crawlers.ex
@@ -6,17 +6,15 @@ defmodule Kwtool.Crawlers do
   alias NimbleCSV.RFC4180, as: CSV
 
   def import_from_file(keyword_file, user) do
-    keywords = keyword_file.path
+    keyword_file.path
     |> File.stream!
     |> CSV.parse_stream(skip_headers: false)
-    |> Enum.each(fn keyword -> add_keyword(List.first(keyword), user) end)
-  end
+    |> Enum.each(fn keyword ->
+      keyword_params = %{phrase: List.first(keyword), status: 0, user: user.id}
+      create_keyword(keyword_params)
+    end)
 
-  def add_keyword(keyword, user) do
-    case create_keyword(%{phrase: keyword, status: 0, user: user.id}) do
-      {:ok, _} -> :ok
-      {:error, _} -> "Failed to insert keyword"
-    end
+    {:ok, "The keyword file is processed successfully!"}
   end
 
   def list_keywords do
