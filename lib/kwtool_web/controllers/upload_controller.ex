82d0e16cb2ac69774eab3052428d7704a496f102
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-26 08:59:39 +0700

    [29] Format code

--- lib/kwtool_web/controllers/upload_controller.ex
+++ lib/kwtool_web/controllers/upload_controller.ex
@@ -1,23 +1,18 @@
 defmodule KwtoolWeb.UploadController do
   use KwtoolWeb, :controller
 
+  alias Kwtool.Crawlers
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def create(conn, %{"keyword_file" => %Plug.Upload{} = keyword_file}) do
-    user = conn.assigns.current_user
-
-    case Kwtool.Crawlers.import_from_file(keyword_file, user) do
+    case Crawlers.import_from_file(keyword_file, conn.assigns.current_user) do
       {:ok, message} ->
         conn
         |> put_flash(:info, message)
         |> redirect(to: Routes.upload_path(conn, :index))
-
-      {:error, _reason} ->
-        conn
-        |> put_flash(:error, _reason)
-        |> index(%{})
     end
   end
 end
