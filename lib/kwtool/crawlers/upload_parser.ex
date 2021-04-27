b2d2db9be30291eb810b6cfd67e70b79c0d0a866
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-27 15:05:48 +0700

    [29] Refactor status into string

--- lib/kwtool/crawlers/upload_parser.ex
+++ lib/kwtool/crawlers/upload_parser.ex
@@ -2,11 +2,18 @@ defmodule Kwtool.Crawlers.UploadParser do
   alias NimbleCSV.RFC4180, as: CSV
 
   def load_from_file(%Plug.Upload{content_type: "text/csv"} = keyword_file) do
-    keyword_file.path
-    |> File.stream!()
-    |> CSV.parse_stream(skip_headers: false)
-    |> Enum.to_list()
+    keyword_list =
+      keyword_file.path
+      |> File.stream!()
+      |> CSV.parse_stream(skip_headers: false)
+      |> Enum.to_list()
+
+    if length(keyword_list) > 0 do
+      {:ok, keyword_list}
+    else
+      {:error, :file_is_empty}
+    end
   end
 
-  def load_from_file(_), do: {:error, "Cannot recognize the file extension"}
+  def load_from_file(_), do: {:error, :file_is_invalid}
 end
--- test/kwtool/crawlers/schemas/keyword_test.exs
+++ test/kwtool/crawlers/schemas/keyword_test.exs
@@ -3,20 +3,36 @@ defmodule Kwtool.Crawlers.Schemas.UserTest do
 
   alias Kwtool.Crawlers.Schemas.Keyword
 
-  describe "keyword/2" do
+  describe "changeset/2" do
     test "returns valid keyword changeset when given valid attributes" do
-      keyword_params = %{phrase: "elixir", status: 1, user_id: 1}
+      keyword_params = %{phrase: "elixir", status: "added", user_id: 1}
       changeset = Keyword.changeset(keyword_params)
 
       assert changeset.valid?
       assert changeset.changes.phrase == "elixir"
+      assert changeset.changes.status == "added"
+      assert changeset.changes.user_id == 1
     end
 
-    test "returns invalid when given valid attributes" do
-      keyword_params = %{phrase: "elixir", status: 1}
+    test "sets default status added when given no status attribute in advance" do
+      keyword_params = %{phrase: "elixir", user_id: 1}
       changeset = Keyword.changeset(keyword_params)
 
+      assert changeset.valid?
+      assert changeset.changes.phrase == "elixir"
+      assert changeset.changes.status == "added"
+      assert changeset.changes.user_id == 1
+    end
+
+    test "returns invalid when given invalid attributes" do
+      changeset = Keyword.changeset(%{})
+
       refute changeset.valid?
+
+      assert errors_on(changeset) == %{
+               phrase: ["can't be blank"],
+               user_id: ["can't be blank"]
+             }
     end
   end
 end
