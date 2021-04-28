44a23b8849c8a3fb2b441c3588f1993c5a9fa4dc
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-28 18:29:29 +0700

    [8] Implement features test for listing keyword page

---
+++ test/kwtool_web/features/keyword_page/view_keyword_page_test.exs
@@ -0,0 +1,12 @@
+defmodule KwtoolWeb.HomePage.ViewKeywordPageTest do
+  use KwtoolWeb.FeatureCase, async: true
+
+  feature "views keyword listing page", %{session: session} do
+    created_user = insert(:user)
+
+    session
+    |> login_as(created_user)
+    |> visit(Routes.keyword_path(KwtoolWeb.Endpoint, :index))
+    |> assert_has(text("Listing Keywords"))
+  end
+end
