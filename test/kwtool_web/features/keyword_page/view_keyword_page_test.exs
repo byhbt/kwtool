b42e417bcf01e607683f4faff08cee1d2c81723c
Thanh Huynh <huynhbathanh@gmail.com>
2021-04-29 08:50:43 +0700

    [8] Implement styling for the details page

--- test/kwtool_web/features/keyword_page/view_keyword_page_test.exs
+++ test/kwtool_web/features/keyword_page/view_keyword_page_test.exs
@@ -9,4 +9,14 @@ defmodule KwtoolWeb.HomePage.ViewKeywordPageTest do
     |> visit(Routes.keyword_path(KwtoolWeb.Endpoint, :index))
     |> assert_has(Query.text("Listing Keywords"))
   end
+
+  feature "views keyword details page", %{session: session} do
+    created_user = insert(:user)
+    keyword = insert(:keyword, user: created_user)
+
+    session
+    |> login_as(created_user)
+    |> visit(Routes.keyword_path(KwtoolWeb.Endpoint, :show, keyword.id))
+    |> assert_has(Query.text("Result for"))
+  end
 end
