73d208c4778d28bc3a60df0259fdf0a396fcca9a
Thanh Huynh <huynhbathanh@gmail.com>
2021-05-05 15:42:43 +0700

    [33] Add tests to check the different between keyword per user

--- test/kwtool_web/features/keyword_page/view_keyword_page_test.exs
+++ test/kwtool_web/features/keyword_page/view_keyword_page_test.exs
@@ -2,12 +2,18 @@ defmodule KwtoolWeb.HomePage.ViewKeywordPageTest do
   use KwtoolWeb.FeatureCase, async: true
 
   feature "views keyword listing page", %{session: session} do
-    created_user = insert(:user)
+    created_user_1 = insert(:user)
+    keyword_of_user_1 = insert(:keyword, user: created_user_1)
+
+    created_user_2 = insert(:user)
+    keyword_of_user_2 = insert(:keyword, user: created_user_2)
 
     session
-    |> login_as(created_user)
+    |> login_as(created_user_1)
     |> visit(Routes.keyword_path(KwtoolWeb.Endpoint, :index))
     |> assert_has(Query.text("Listing Keywords"))
+    |> assert_has(Query.text(keyword_of_user_1.phrase))
+    |> refute_has(Query.text(keyword_of_user_2.phrase))
   end
 
   feature "views keyword details page", %{session: session} do
@@ -18,5 +24,19 @@ defmodule KwtoolWeb.HomePage.ViewKeywordPageTest do
     |> login_as(created_user)
     |> visit(Routes.keyword_path(KwtoolWeb.Endpoint, :show, keyword.id))
     |> assert_has(Query.text("Result for"))
+  end
+
+  feature "redirects to listing keywords page when given other user keyword", %{session: session} do
+    created_user_1 = insert(:user)
+    keyword_of_user_1 = insert(:keyword, user: created_user_1)
+
+    created_user_2 = insert(:user)
+    keyword_of_user_2 = insert(:keyword, user: created_user_2)
+
+    session
+    |> login_as(created_user_1)
+    |> visit(Routes.keyword_path(KwtoolWeb.Endpoint, :show, keyword_of_user_2.id))
+    |> assert_has(Query.text(keyword_of_user_1.phrase))
+    |> assert_has(Query.text("Listing Keywords"))
   end
 end
