9e6356d9c1459d185254065ff9b9f6a2a7b44cc2
Thanh Huynh <huynhbathanh@gmail.com>
2022-01-19 15:57:07 +0700

    [40] Add test error handler

--- test/kwtool/account/error_handler_test.exs
+++ test/kwtool/account/error_handler_test.exs
@@ -1,5 +1,23 @@
 defmodule Kwtool.Account.ErrorHandlerTest do
-  use Kwtool.DataCase, async: true
+  use KwtoolWeb.ConnCase, async: true
 
   alias Kwtool.Account.ErrorHandler
+
+  describe "auth_error/2" do
+    test "renders 401 error page", %{conn: conn} do
+      conn = ErrorHandler.auth_error(conn, {401, ""}, "")
+
+      assert %{
+               "errors" => [
+                 %{
+                   "code" => "unauthorized",
+                   "detail" => %{},
+                   "message" => "Unauthorized"
+                 }
+               ]
+             } = json_response(conn, 401)
+
+      assert conn.halted
+    end
+  end
 end
