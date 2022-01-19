ce01db385b323ed046e2eb288939047405f43091
Thanh Huynh <huynhbathanh@gmail.com>
2022-01-19 15:39:30 +0700

    [40] Add test for Gaurdian

--- lib/kwtool/account/error_handler.ex
+++ lib/kwtool/account/error_handler.ex
@@ -1,16 +1,12 @@
-defmodule Kwtool.Account.Guardian do
-  use Guardian, otp_app: :kwtool
+defmodule Kwtool.Account.ErrorHandler do
+  @behaviour Guardian.Plug.ErrorHandler
 
-  alias Kwtool.Account.Users
+  use KwtoolWeb, :controller
 
-  def subject_for_token(user, _claims) do
-    {:ok, to_string(user.id)}
-  end
-
-  def resource_from_claims(%{"sub" => id}) do
-    user = Users.get_user!(id)
-    {:ok, user}
-  rescue
-    Ecto.NoResultsError -> {:error, :resource_not_found}
+  @impl Guardian.Plug.ErrorHandler
+  def auth_error(conn, {type, _reason}, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(401, to_string(type))
   end
 end
--- lib/kwtool/account/guardian.ex
+++ lib/kwtool/account/guardian.ex
@@ -1,12 +1,16 @@
-defmodule Kwtool.Account.ErrorHandler do
-  @behaviour Guardian.Plug.ErrorHandler
+defmodule Kwtool.Account.Guardian do
+  use Guardian, otp_app: :kwtool
 
-  use KwtoolWeb, :controller
+  alias Kwtool.Account.Users
 
-  @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, {type, _reason}, _opts) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(401, to_string(type))
+  def subject_for_token(user, _claims) do
+    {:ok, to_string(user.id)}
+  end
+
+  def resource_from_claims(%{"sub" => id}) do
+    user = Users.get_user!(id)
+    {:ok, user}
+  rescue
+    Ecto.NoResultsError -> {:error, :resource_not_found}
   end
 end
