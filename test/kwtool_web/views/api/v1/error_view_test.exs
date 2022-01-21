5899787762e9e40caf9c4e8152ddd05b6e1e2efd
Thanh Huynh <huynhbathanh@gmail.com>
2022-01-21 17:50:48 +0700

    [40] Add describe for error_view test

--- test/kwtool_web/views/api/v1/error_view_test.exs
+++ test/kwtool_web/views/api/v1/error_view_test.exs
@@ -34,63 +34,67 @@ defmodule KwtoolWeb.Api.V1.ErrorViewTest do
     end
   end
 
-  test "renders 404.json" do
-    assert render(ErrorView, "404.json", []) == %{
-             errors: [%{code: :not_found, detail: %{}, message: "Not Found"}]
-           }
-  end
+  describe "render/3" do
+    test "renders 404.json" do
+      assert render(ErrorView, "404.json", []) == %{
+               errors: [%{code: :not_found, detail: %{}, message: "Not Found"}]
+             }
+    end
 
-  test "renders 500.json" do
-    assert render(ErrorView, "500.json", []) == %{
-             errors: [
-               %{code: :internal_server_error, detail: %{}, message: "Internal Server Error"}
-             ]
-           }
-  end
-
-  test "renders custom error message" do
-    assert render(ErrorView, "500.json", status: 500, message: "Something went wrong") ==
-             %{
+    test "renders 500.json" do
+      assert render(ErrorView, "500.json", []) == %{
                errors: [
-                 %{code: :internal_server_error, detail: %{}, message: "Something went wrong"}
+                 %{code: :internal_server_error, detail: %{}, message: "Internal Server Error"}
                ]
              }
+    end
+
+    test "given error code and an invalid changeset with multiple errors fields, renders error.json" do
+      changeset = Device.changeset(%{})
+      error = %{code: :validation_error, changeset: changeset}
+
+      assert render(ErrorView, "error.json", error) ==
+               %{
+                 errors: [
+                   %{
+                     code: :validation_error,
+                     detail: %{
+                       device_id: ["can't be blank"],
+                       device_name: ["can't be blank"],
+                       operating_system: ["can't be blank"]
+                     },
+                     message:
+                       "Device can't be blank, Device name can't be blank and Operating system can't be blank"
+                   }
+                 ]
+               }
+    end
+
+    test "given error code and an invalid changeset with single error field, renders error.json" do
+      changeset = Device.changeset(%{device_id: "12345678-9012", device_name: "Android"})
+      error = %{code: :validation_error, changeset: changeset}
+
+      assert render(ErrorView, "error.json", error) ==
+               %{
+                 errors: [
+                   %{
+                     code: :validation_error,
+                     detail: %{operating_system: ["can't be blank"]},
+                     message: "Operating system can't be blank"
+                   }
+                 ]
+               }
+    end
   end
 
-  test "given error code and an invalid changeset with multiple errors fields, renders error.json" do
-    changeset = Device.changeset(%{})
-    error = %{code: :validation_error, changeset: changeset}
-
-    assert render(ErrorView, "error.json", error) ==
-             %{
-               errors: [
-                 %{
-                   code: :validation_error,
-                   detail: %{
-                     device_id: ["can't be blank"],
-                     device_name: ["can't be blank"],
-                     operating_system: ["can't be blank"]
-                   },
-                   message:
-                     "Device can't be blank, Device name can't be blank and Operating system can't be blank"
-                 }
-               ]
-             }
-  end
-
-  test "given error code and an invalid changeset with single error field, renders error.json" do
-    changeset = Device.changeset(%{device_id: "12345678-9012", device_name: "Android"})
-    error = %{code: :validation_error, changeset: changeset}
-
-    assert render(ErrorView, "error.json", error) ==
-             %{
-               errors: [
-                 %{
-                   code: :validation_error,
-                   detail: %{operating_system: ["can't be blank"]},
-                   message: "Operating system can't be blank"
-                 }
-               ]
-             }
+  describe "render/4" do
+    test "renders custom error message" do
+      assert render(ErrorView, "500.json", status: 500, message: "Something went wrong") ==
+               %{
+                 errors: [
+                   %{code: :internal_server_error, detail: %{}, message: "Something went wrong"}
+                 ]
+               }
+    end
   end
 end
