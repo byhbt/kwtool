e097e44580a1e667b8c462cae83ab629156c378c
Thanh Huynh <huynhbathanh@gmail.com>
2022-02-16 17:53:32 +0700

    [7] Update web controller

--- lib/kwtool_web/controllers/upload_controller.ex
+++ lib/kwtool_web/controllers/upload_controller.ex
@@ -2,21 +2,28 @@ defmodule KwtoolWeb.UploadController do
   use KwtoolWeb, :controller
 
   alias Kwtool.Crawler.Keywords
+  alias KwtoolWeb.Api.V1.UploadParams
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
-  def create(conn, %{"keyword_file" => %Plug.Upload{} = keyword_file}) do
-    {alert_type, alert_message} =
-      case Keywords.save_keywords_list(keyword_file, conn.assigns.current_user) do
-        {:ok, :file_is_processed} -> {:info, "The keyword file is processed successfully!"}
-        {:error, :file_is_empty} -> {:error, "The keyword file is empty!"}
-        {:error, :file_is_invalid} -> {:error, "The keyword file is invalid!"}
-      end
+  def create(conn, params) do
+    with {:ok, validated_params} <- ParamsValidator.validate(params, for: UploadParams),
+         {:ok, :file_is_processed} <-
+           Keywords.save_keywords_list(validated_params, conn.assigns.current_user) do
+      conn
+      |> put_flash(:info, "The keyword file is processed successfully!")
+      |> redirect(to: Routes.upload_path(conn, :index))
+    else
+      {:error, :file_is_empty} ->
+        {:error, :upload_error, "The keyword file is empty!"}
 
-    conn
-    |> put_flash(alert_type, alert_message)
-    |> redirect(to: Routes.upload_path(conn, :index))
+      {:error, :file_is_invalid} ->
+        {:error, :file_is_invalid, "The keyword file is empty!"}
+
+      error ->
+        error
+    end
   end
 end
