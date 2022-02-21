b4bc9c88ba952cbd5c66380557d35265af03afa6
Thanh Huynh <huynhbathanh@gmail.com>
2022-02-21 09:42:09 +0700

    [38] Add test for keyword listing

--- test/kwtool_web/controllers/api/v1/keyword_controller_test.exs
+++ test/kwtool_web/controllers/api/v1/keyword_controller_test.exs
@@ -31,5 +31,30 @@ defmodule KwtoolWeb.Api.V1.KeywordControllerTest do
                "included" => []
              } = json_response(conn, 200)
     end
+
+    test "returns an empty list given user does NOT has any keyword", %{conn: conn} do
+      created_user = insert(:user)
+
+      conn =
+        conn
+        |> with_signed_in_user(created_user)
+        |> get(Routes.api_keyword_path(conn, :index))
+
+      assert %{"data" => [], "included" => []} = json_response(conn, 200)
+    end
+
+    test "returns unauthorized ", %{conn: conn} do
+      conn = get(conn, Routes.api_keyword_path(conn, :index))
+
+      assert %{
+               "errors" => [
+                 %{
+                   "code" => "unauthorized",
+                   "detail" => %{},
+                   "message" => "Unauthorized"
+                 }
+               ]
+             } = json_response(conn, 401)
+    end
   end
 end
