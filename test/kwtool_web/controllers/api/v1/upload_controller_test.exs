7f8fb0812a484b2979b6df509ec8bf2fd790d731
Thanh Huynh <huynhbathanh@gmail.com>
2022-02-26 10:40:27 +0700

    [7] Update success upload message

--- test/kwtool_web/controllers/api/v1/upload_controller_test.exs
+++ test/kwtool_web/controllers/api/v1/upload_controller_test.exs
@@ -4,7 +4,7 @@ defmodule KwtoolWeb.Api.V1.UploadControllerTest do
   import KwtoolWeb.Support.ConnHelper
 
   describe "post create/2" do
-    test "given the import processed successfully, return 201 status with success message", %{
+    test "given the import processed successfully, returns 201 status with success message", %{
       conn: conn
     } do
       created_user = insert(:user)
@@ -18,7 +18,7 @@ defmodule KwtoolWeb.Api.V1.UploadControllerTest do
       assert %{
                "data" => %{
                  "attributes" => %{
-                   "message" => "file_is_processed"
+                   "message" => "Keyword file is processed successfully"
                  },
                  "id" => _,
                  "relationships" => %{},
@@ -26,17 +26,6 @@ defmodule KwtoolWeb.Api.V1.UploadControllerTest do
                },
                "included" => []
              } = json_response(conn, 201)
-    end
-
-    test "given unauthenticated request, return 401 status with error", %{conn: conn} do
-      post_params = %{:keyword_file => fixture_file_upload("3-keywords.csv")}
-      conn = post(conn, Routes.api_upload_path(conn, :create), post_params)
-
-      assert %{
-               "errors" => [
-                 %{"code" => "unauthorized", "detail" => %{}, "message" => "Unauthorized"}
-               ]
-             } = json_response(conn, 401)
     end
 
     test "given an empty file, returns 422 status with error", %{conn: conn} do
@@ -77,6 +66,17 @@ defmodule KwtoolWeb.Api.V1.UploadControllerTest do
                  }
                ]
              } = json_response(conn, 422)
+    end
+
+    test "given unauthenticated upload request, returns 401 status with error", %{conn: conn} do
+      post_params = %{:keyword_file => fixture_file_upload("3-keywords.csv")}
+      conn = post(conn, Routes.api_upload_path(conn, :create), post_params)
+
+      assert %{
+               "errors" => [
+                 %{"code" => "unauthorized", "detail" => %{}, "message" => "Unauthorized"}
+               ]
+             } = json_response(conn, 401)
     end
   end
 end
