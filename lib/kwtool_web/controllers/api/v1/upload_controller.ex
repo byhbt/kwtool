c0e6216dcaf5fa931b941b9df62e7a9a5365cb9e
Thanh Huynh <huynhbathanh@gmail.com>
2022-02-26 10:40:27 +0700

    [7] Update success upload message

--- lib/kwtool_web/controllers/api/v1/upload_controller.ex
+++ lib/kwtool_web/controllers/api/v1/upload_controller.ex
@@ -9,10 +9,15 @@ defmodule KwtoolWeb.Api.V1.UploadController do
 
     with {:ok, %{keyword_file: %Plug.Upload{} = keyword_file}} <-
            ParamsValidator.validate(params, for: UploadParams),
-         {:ok, success_message} <- Keywords.save_keywords_list(keyword_file, user) do
+         {:ok, _message} <- Keywords.save_keywords_list(keyword_file, user) do
       conn
       |> put_status(:created)
-      |> render("show.json", %{data: %{id: :os.system_time(:millisecond), message: success_message}})
+      |> render("show.json", %{
+        data: %{
+          id: :os.system_time(:millisecond),
+          message: "Keyword file is processed successfully"
+        }
+      })
     end
   end
 end
