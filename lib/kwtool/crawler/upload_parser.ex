e408b241a364ecded27ca8e3b537a570499b9e6d
Thanh Huynh <huynhbathanh@gmail.com>
2022-03-03 16:54:41 +0700

    [7] Remove used method on UploadParser

--- lib/kwtool/crawler/upload_parser.ex
+++ lib/kwtool/crawler/upload_parser.ex
@@ -16,10 +16,4 @@ defmodule Kwtool.Crawler.UploadParser do
   end
 
   def parse!(_), do: raise("Uploaded keyword file is invalid")
-
-  def parse_asset_type(%Plug.Upload{content_type: "csv" <> _}), do: {:ok, :csv}
-  def parse_asset_type(_), do: {:error, :unsupported_asset_type}
-
-  def supported_mime_types, do: ~w(text/csv)
-  def max_file_size_in_bytes, do: 3_000_000
 end
