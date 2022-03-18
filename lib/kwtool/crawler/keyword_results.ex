3ea5ca88754f38ce8c69fb5c6b6734fa7790148a
Thanh Huynh <huynhbathanh@gmail.com>
2022-03-18 15:39:51 +0700

    [36] Implement search by organic result

--- lib/kwtool/crawler/keyword_results.ex
+++ lib/kwtool/crawler/keyword_results.ex
@@ -2,16 +2,18 @@ defmodule Kwtool.Crawler.KeywordResults do
   import Ecto.Query, warn: false
 
   alias Kwtool.Account.Schemas.User
-  # alias Kwtool.Crawler.Schemas.{Keyword, KeywordResult}
+  alias Kwtool.Crawler.Schemas.{Keyword, KeywordResult}
   alias Kwtool.Repo
 
   def search(%User{} = user, params \\ %{}) do
-    search_phrase = get_in(params, ["query"])
-    wildcard_search = "%#{search_phrase}%"
+    search_phrase = get_in(params, ["url"])
+    url_wildcard = "%#{search_phrase}%"
 
     Keyword
+    |> join(:inner, [k], kr in KeywordResult, on: kr.keyword_id == k.id)
     |> where([k], k.user_id == ^user.id)
-    |> where([k], ilike(k.phrase, ^wildcard_search))
+    |> where([kr], fragment("organic_result_urls ::text ilike ANY(array[?::text])", ^url_wildcard))
+    |> preload(:keyword_results)
     |> Repo.all()
   end
 end
