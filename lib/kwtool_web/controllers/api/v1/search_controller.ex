f5a1efc1263ef82daa4f4fce6e290670abddaaa5
Thanh Huynh <huynhbathanh@gmail.com>
2022-03-28 23:05:35 +0700

    [36] Implement search query

--- lib/kwtool_web/controllers/api/v1/search_controller.ex
+++ lib/kwtool_web/controllers/api/v1/search_controller.ex
@@ -2,15 +2,15 @@ defmodule KwtoolWeb.Api.V1.SearchController do
   use KwtoolWeb, :api_controller
 
   alias Kwtool.Crawler.KeywordResults
+  alias KwtoolWeb.Api.V1.SearchParams
 
   def index(conn, params) do
-    keywords =
+    # TODO, find a way to get current user
+    with {:ok, validated_params} <- ParamsValidator.validate(params, for: SearchParams),
+          keywords <- KeywordResults.search(Guardian.Plug.current_resource(conn), validated_params) do
       conn
-      |> Guardian.Plug.current_resource()
-      |> KeywordResults.search(params)
-
-    conn
-    |> put_status(:ok)
-    |> render("show.json", %{data: keywords})
+      |> put_status(:ok)
+      |> render("show.json", %{data: keywords})
+    end
   end
 end
