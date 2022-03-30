61c1c0516af92b1955659d8c6b271960eb81ec1f
Thanh Huynh <huynhbathanh@gmail.com>
2022-03-30 17:11:52 +0700

    [36] Handle empty search with params case

--- test/kwtool/crawler/keyword_results_test.exs
+++ test/kwtool/crawler/keyword_results_test.exs
@@ -11,6 +11,7 @@ defmodule Kwtool.KeywordResultsTest do
 
       [keyword_result] = KeywordResults.search(user, %{keyword: "coffee"})
 
+      assert Enum.count(keyword_result.keyword_results) == 1
       assert keyword.id == keyword_result.id
       assert keyword.phrase == "coffee"
     end
@@ -22,6 +23,20 @@ defmodule Kwtool.KeywordResultsTest do
 
       [keyword_result] = KeywordResults.search(user, %{keyword: "coffee", url: "star-coffee.com"})
 
+      assert Enum.count(keyword_result.keyword_results) == 1
+      assert keyword.id == keyword_result.id
+      assert keyword.phrase == "coffee"
+    end
+
+    test "given an existing keyword has multiple crawl results, returns the matching keyword and url" do
+      user = insert(:user)
+      keyword = insert(:keyword, phrase: "coffee", status: "finished", user: user)
+      insert(:keyword_result, keyword: keyword, organic_result_urls: ["https://star-coffee.com"])
+      insert(:keyword_result, keyword: keyword, organic_result_urls: ["https://black-coffee.com"])
+
+      [keyword_result] = KeywordResults.search(user, %{keyword: "coffee", url: "coffee"})
+
+      assert Enum.count(keyword_result.keyword_results) == 2
       assert keyword.id == keyword_result.id
       assert keyword.phrase == "coffee"
     end
@@ -37,20 +52,18 @@ defmodule Kwtool.KeywordResultsTest do
       assert keyword.phrase == "coffee"
     end
 
+    test "given an empty param, returns an empty list" do
+      user = insert(:user)
+
+      assert KeywordResults.search(user, %{}) == []
+    end
+
     test "given a keyword does NOT exist, returns an empty list" do
       user = insert(:user)
       keyword = insert(:keyword, phrase: "coffee", status: "finished", user: user)
       insert(:keyword_result, keyword: keyword, all_ads_count: 12)
 
       assert KeywordResults.search(user, %{keyword: "latte"}) == []
-    end
-
-    test "given an empty params, returns the matching keyword result" do
-      user = insert(:user)
-
-      assert_raise FunctionClauseError, fn ->
-        KeywordResults.search(user, %{}) == []
-      end
     end
   end
 end
